d63e03d5b49e89ed64afd88c89cd9888581827bd
b-the-coder <binhe1989@gmail.com>
2026-03-05 13:11:07 -0500

    set up ESLint and prettier

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-console.log("script loaded")
+console.log("script loaded");
 const person = {
-    Name: "Emma",
-    Email: "emmahsde@gmail.com",
-    Area:"NY/NJ",
-    Aboutme:"Hi, my name is Emma.",
-    ProExp: ["Attend JS bootcamp.","Worked as a chemical analyst."]
+  Name: "Emma",
+  Email: "emmahsde@gmail.com",
+  Area: "NY/NJ",
+  Aboutme: "Hi, my name is Emma.",
+  ProExp: ["Attend JS bootcamp.", "Worked as a chemical analyst."],
+};
 
-}
-
-const person_name = document.getElementById('name');
+const person_name = document.getElementById("name");
 person_name.textContent = person.Name;
 
-const person_email = document.getElementById('email');
+const person_email = document.getElementById("email");
 person_email.textContent = person.Email;
 
-const person_area = document.getElementById('area');
+const person_area = document.getElementById("area");
 person_area.textContent = person.Area;
 
-const person_aboutme = document.getElementById('aboutme');
+const person_aboutme = document.getElementById("aboutme");
 person_aboutme.textContent = person.Aboutme;
 
-const person_proexp1 = document.getElementById('proexp1');
+const person_proexp1 = document.getElementById("proexp1");
 person_proexp1.textContent = person.ProExp[0];
 
-const person_proexp2 = document.getElementById('proexp2');
+const person_proexp2 = document.getElementById("proexp2");
 person_proexp2.textContent = person.ProExp[1];
 
-const contact = document.getElementById("contactinfo")
-contact.textContent = contact.textContent + person.Email
+const contact = document.getElementById("contactinfo");
+contact.textContent = contact.textContent + person.Email;
+
+const happy = "happywife, happylife"
