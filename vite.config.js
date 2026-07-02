5a27d4823fb4a4700ed59c543c59d661cf32009d
b-the-coder <binhe1989@gmail.com>
2026-07-02 13:02:28 -0400

    Add test coverage thresholds and scripts for test coverage check

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     coverage: {
-      provider: "v8", // or 'istanbul'
+      provider: "v8",
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
     },
   },
 });
